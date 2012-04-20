6633589763cf15e4167e274fe7c4bf37c73fa85f
planet95 <planet95@gmail.com>
2012-04-19 20:35:21 -0700

    one change 50000 lives lost.
    Change-Id: I0a2d55ab76fbeedf7ee031f656e63a093dff6569
    Signed-off-by: planet95 <planet95@gmail.com>

---
+++ src/en/ventura/tnc/VenturaActivity.java
@@ -0,0 +1,17 @@
+package en.ventura.tnc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class VenturaActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, Android 50000");
+        setContentView(tv);
+        
+    }
+}
